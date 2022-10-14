d01f6e471a7f04ebfbb58b787b24efa6a6475aa9
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-14 13:44:46 +0800

    Update TopRecords_ByWeight.py

--- pages/TopRecords_ByWeight.py
+++ pages/TopRecords_ByWeight.py
@@ -8,7 +8,7 @@ import numpy as np
 
 df = 'https://raw.githubusercontent.com/Ardmono/APU-Streamlit/main/filename.csv'
 df = pd.read_csv(df)
-df = df.drop(columns=['Age', 'Team', 'BirthYear', 'BirthDate','Country','State','Place','Equipment','meetid','MeetCountry'])
+df = df.drop(columns=['Age', 'Team', 'BirthYear', 'BirthDate','Country','State','Place','meetid','MeetCountry'])
 df['Year'] = pd.DatetimeIndex(df['Date']).year
 df.fillna(0, inplace=True)
 
