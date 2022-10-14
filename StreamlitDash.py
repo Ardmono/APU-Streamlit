e7f16344d62a6c48d611417e176ddcf8728ff2d9
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-14 11:09:41 +0800

    Update StreamlitDash.py

--- StreamlitDash.py
+++ StreamlitDash.py
@@ -26,6 +26,24 @@ st.set_page_config(layout="wide")
 
 
 
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
 #st.header(body='APU Australia')
 #st.image(img,width=150)
 #st.text('Placeholder 1 ')
@@ -41,6 +59,7 @@ st.header(body='APU Australia')
 ####Button Filters###
 years = ['2018','2019','2020','2021','2022']
 test = df['Date'].min
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 st.sidebar.image(img,width=200)   
 #st.sidebar.text('Strength Club')   
 #st.sidebar.header("Google.com")
@@ -94,23 +113,4 @@ st.text(elapsed_time)
 
 st.text(df.dtypes)
 
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
 
-def page2():
-    st.markdown("# Page 2 ❄️")
-    st.sidebar.markdown("# Page 2 ❄️")
-
-def page3():
-    st.markdown("# Page 3 🎉")
-    st.sidebar.markdown("# Page 3 🎉")
-
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
-}
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
