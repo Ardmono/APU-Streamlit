e7cf00021017c32d8075b12234feec2a2e2f14d5
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-14 12:54:44 +0800

    Update TopRecords_ByWeight.py

--- pages/TopRecords_ByWeight.py
+++ pages/TopRecords_ByWeight.py
@@ -14,9 +14,12 @@ df.fillna(0, inplace=True)
 
 task = df['WeightClassKg'].unique()
 for i in range(len(task)):
-  st.checkbox(label=(task[i]))
+  col1, col2, col3 = st.columns(3)
+  col1.metric("Temperature", "70 °F", "1.2 °F")
+  col2.metric("Wind", "9 mph", "-8%")
+  col3.metric("Humidity", "86%", "4%")
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+# col1, col2, col3 = st.columns(3)
+# col1.metric("Temperature", "70 °F", "1.2 °F")
+# col2.metric("Wind", "9 mph", "-8%")
+# col3.metric("Humidity", "86%", "4%")
