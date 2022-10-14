9b7c48897a4f155646270f05e0ddcb75929deffd
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-14 12:52:25 +0800

    Update StrengthCompare.py

--- pages/StrengthCompare.py
+++ pages/StrengthCompare.py
@@ -17,7 +17,9 @@ df.fillna(0, inplace=True)
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 #st.metric('Best Bench', value, delta=None, delta_color="normal", help=None)
 
-
+task = df['BodyweightKg'].unique()
+for i in range(len(task)):
+  st.checkbox(label=(task[i]))
 bodyweight = st.number_input("Bodyweight")
 col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
