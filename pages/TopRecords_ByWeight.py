239e658fef0bc4d7dd85446b5a31ebf3b161f7c6
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-14 13:16:58 +0800

    Update TopRecords_ByWeight.py

--- pages/TopRecords_ByWeight.py
+++ pages/TopRecords_ByWeight.py
@@ -20,7 +20,7 @@ for i in range(len(task)):
     print(rslt_df)
     rslt_df['Best3BenchKg'].max()
     #st.metric(label='Bench',value=(rslt_df['Best3BenchKg'].max()))
-    col4, col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(3)
     col1.metric("Bench", (rslt_df['Best3BenchKg'].max()), "1.2 °F")
     col2.metric("Wind", "9 mph", "-8%")
 
