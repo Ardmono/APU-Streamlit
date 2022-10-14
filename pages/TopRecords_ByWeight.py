8ac47a6775e8da0e8920af346cd50a88a606e5b7
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-14 12:51:08 +0800

    Create TopRecords_ByWeight.py

---
+++ pages/TopRecords_ByWeight.py
@@ -0,0 +1,13 @@
+from time import strptime
+import pandas as pd 
+import streamlit as st
+from datetime import datetime
+import time
+import numpy as np
+
+
+df = 'https://raw.githubusercontent.com/Ardmono/APU-Streamlit/main/filename.csv'
+df = pd.read_csv(df)
+df = df.drop(columns=['Age', 'Team', 'BirthYear', 'BirthDate','Country','State','Place','Equipment','meetid','MeetCountry'])
+df['Year'] = pd.DatetimeIndex(df['Date']).year
+df.fillna(0, inplace=True)
