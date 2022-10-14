ee6271a7bcb2d450bce8a1a8d5ecfb8f5e1aa22e
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-14 11:15:03 +0800

    Update StrengthCompare.py

--- pages/StrengthCompare.py
+++ pages/StrengthCompare.py
@@ -5,6 +5,18 @@ from datetime import datetime
 import time
 import numpy as np
 
+
+df = 'https://raw.githubusercontent.com/Ardmono/APU-Streamlit/main/filename.csv'
+df = pd.read_csv(df)
+df = df.drop(columns=['Age', 'Team', 'BirthYear', 'BirthDate','Country','State','Place','Equipment','meetid','MeetCountry'])
+df['Year'] = pd.DatetimeIndex(df['Date']).year
+df.fillna(0, inplace=True)
+
+benchtotal = st.number_input("Bench Best")
+#if len(benchtotal) == 0:
+#    benchtotal = df['MeetState']
+
+
 def main_page():
     st.markdown("# Main page 🎈")
     st.sidebar.markdown("# Main page 🎈")
