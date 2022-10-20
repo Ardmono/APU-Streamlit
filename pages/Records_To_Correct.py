f1f94577669dc5a0ed9ea3ae7ce03b556bb4e9cd
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-20 23:47:29 +0800

    Update Records_To_Correct.py

--- pages/Records_To_Correct.py
+++ pages/Records_To_Correct.py
@@ -11,4 +11,21 @@ import plotly.graph_objects as go
 df = 'https://raw.githubusercontent.com/Ardmono/APU-Streamlit/main/filename.csv'
 df = pd.read_csv(df)
 
-print(df)
+#print(df)
+
+#len(df.query('MeetName < 1'))
+print(df[['MeetName', 'MeetState', 'MeetTown']])
+df.fillna(value='Unknown',inplace=True)
+
+print(df[['MeetName', 'MeetState', 'MeetTown']])
+
+#nan_in_col  = df[df['MeetName'].isna()]
+df1 = df[(df.MeetState == 'Unknown') | (df.MeetTown == 'Unknown') | (df.MeetState == 'Unknown')]  #& (df.MeetName == 0)]
+
+#,'Bench1Kg', 'Bench2Kg', 'Bench3Kg' ,'Best3BenchKg' ,'Deadlift1Kg', 'Deadlift2Kg' ,'Deadlift3Kg', 'Best3DeadliftKg'
+df1 = df1.drop(columns=['Age', 'Team', 'BirthYear', 'BirthDate','Country','State','Place','Name', 'WeightClassKg', 'BodyweightKg', 'Division', 'Squat1Kg', 'Squat2Kg', 'Squat3Kg', 'Best3SquatKg','Bench1Kg', 'Bench2Kg', 'Bench3Kg' ,'Best3BenchKg' ,'Deadlift1Kg', 'Deadlift2Kg' ,'Deadlift3Kg', 'Best3DeadliftKg','TotalKg'])
+##a = df.query('a.isnull()', engine='python')
+
+print(df1)
+
+st.dataframe(df1)
