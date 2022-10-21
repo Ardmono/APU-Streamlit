bbf7f412cf8d2fcc4199ebbd3a1567f3eaf9abff
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-21 12:23:14 +0800

    Update Competition_Numbers.py

--- pages/Competition_Numbers.py
+++ pages/Competition_Numbers.py
@@ -19,9 +19,9 @@ df1 = df
 
  
 df = df1
-meetName = st.sidebar.multiselect("State",options=('NSW' ,'QLD' ,'WA', 'VIC' ,'ACT' , 'SA' ,'TAS'))
-if len(meetName) == 0:
-    meetName = df['MeetName']
+meetState = st.sidebar.multiselect("State",options=('NSW' ,'QLD' ,'WA', 'VIC' ,'ACT' , 'SA' ,'TAS'))
+if len(meetState) == 0:
+    meetState = df['MeetState']
     
 col1, col2,col4,col5 = st.columns(4)
 with col1:
@@ -43,7 +43,10 @@ eventy = st.sidebar.radio("Event",options=('SBD', 'B', 'BD', 'D','All'))
 #    eventy = 'SBD'
 if eventy == 'All':
     eventy = df['Event']
-st.header('Competition Recap, defaults is latest comp ')
+st.header('Competition Numbers - You can group by State or Year')
+meetName = st.sidebar.multiselect("Meet Name",options=df['MeetName'].unique())
+if len(meetName) == 0:
+    meetName = df['MeetName']
 maxmeet = max(df.MeetName.apply(len))
 meeter = meetName
 if len(meeter) > maxmeet+1 :
@@ -69,41 +72,14 @@ else:
 # if len(state) == 0:
 #     state = df['MeetState']
 #print(df1.iloc[:,26:35])
-# df_exec = df.query(
-#    "Sex==@sex_input &  WeightClassKg==@weight_input & Equipment == @equpped & MeetState == @state & MeetName == @meetName & Event == @eventy"
-# )
+df_exec = df.query(
+   "Sex==@sex_input &  WeightClassKg==@weight_input & Equipment == @equpped & MeetState == @meetState & MeetName == @meetName & Event == @eventy"
+)
 
-# #df_filtered = df_exec.query(len('Squat1Kg > 0 & Squat2Kg > 0 & Squat3Kg > 0'))
-# #['Squat1Kg' , 'Squat2Kg'  ,'Squat3Kg' ,  'Bench1Kg',  'Bench2Kg' ,'Bench3Kg',  'Deadlift1Kg',  'Deadlift2Kg', 'Deadlift3Kg']
-# #st.metric('All good squats', value=len(df_exec.query('Sex == 'F'')), delta=None, delta_color="normal", help=None)
-# dfsex = df.query(
-#    "WeightClassKg==@weight_input & Equipment == @equpped & MeetState == @state & MeetName == @meetName & Event == @eventy"
-# )
-
-
-# lifters = len(dfsex[dfsex['Sex'] =='F']) + len(dfsex[dfsex['Sex'] =='M'])
-# #print(lifters)
-# #print(df_exec[df_exec['Sex'] =='F'].count())
-# #st.metric('All good squats', value=len(df_exec.query('Sex == 'M'')), delta=None, delta_color="normal", help=None)
-# # st.metric(label="Count of failed Bench3",value=len(df_exec.query('Bench3Kgfail > 0')))
-# a, b,c = st.columns(3,gap='Small')
-# a.metric(label="Female Lifters", value=len(dfsex[df['Sex'] =='F']))
-# b.metric(label="Male Lifters", value=len(dfsex[df['Sex'] =='M']))
-# c.metric(label="Total Lifters", value=lifters)
-# #d.metric(label="Total KG of all lifted weights", value=(str(df_exec['TotalKg'].sum())+' Kg'))
-# st.text('Current Comp: '+str(meeter))
-# col1, col2, col3,col4,col5 = st.columns(5,gap='Medium')
-# col1.metric(label="How Many Lifters", value=len(df_exec.query('Bench3Kgfail > 0')))
-# col2.metric(label="Gender Ratios", value='Bleh')
-# col3.metric(label="% Succesful Lifts", value='Bleh')
-# col4.metric(label="Total KG of all lifted weights", value=(str(df_exec['TotalKg'].sum())+' Kg'))
-# col5.metric(label="Count of 9/9 Lifts",value=len(df_exec.query('Squat3Kg > 1 & Squat2Kg > 1 & Squat1Kg > 1 & Bench1Kg > 1 & Bench2Kg > 1 & Bench3Kg > 1 & Deadlift1Kg > 1 & Deadlift2Kg > 1 & Deadlift3Kg > 1')))
-
-
-
-
+df_exec['count'] = df_exec.groupby('MeetState')['MeetState'].transform('count')
+df1 =df_exec[['MeetName', 'MeetState', 'MeetTown','count']]
 #How many lifters, genders, weights, lifts, ----------------5/10
-
+st.dataframe(df1,width=20000)
 #Failed Lifts % Breakdown ---------------8/10 
 
 #Number of lifters who went 9/9--------------------6/10
