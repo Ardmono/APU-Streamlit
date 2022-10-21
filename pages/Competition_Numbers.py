efed4219cb3573b43c409f71d086276c053654ac
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-21 13:01:52 +0800

    Update Competition_Numbers.py

--- pages/Competition_Numbers.py
+++ pages/Competition_Numbers.py
@@ -7,7 +7,7 @@ import time
 import numpy as np
 import plotly.graph_objects as go
 
-
+st.set_page_config(layout="wide")
 
 df = 'https://raw.githubusercontent.com/Ardmono/APU-Streamlit/main/filename.csv'
 df = pd.read_csv(df)
