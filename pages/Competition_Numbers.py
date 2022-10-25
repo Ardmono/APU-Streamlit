1a6ffa6e13bca41c144ff372e9cceb3883ff5bf5
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-25 13:50:06 +0800

    Update Competition_Numbers.py

--- pages/Competition_Numbers.py
+++ pages/Competition_Numbers.py
@@ -112,7 +112,7 @@ if grpst == 'True':
         df_exec['count'] = df_exec.groupby('MeetState')['meetid'].transform('count')
         df_exec.drop_duplicates(subset="MeetState",
                         keep='first', inplace=True)
-        df1 =df_exec[['MeetState', 'MeetTown','Year','count','State_Year']]
+        df1 =df_exec[['MeetState', 'MeetTown','Year','count']]
     #@pass
 elif grpst == 'False':
     if grpyr == 'True':
