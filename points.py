7c6465e1159e9cd6ca6fea6b6468272bf60ab1a1
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-25 14:32:51 +0800

    added coefficient

---
+++ points.py
@@ -0,0 +1,73 @@
+
+import pandas as pd
+import sys
+#from coefficient import glossbrenner, wilks, mcculloch, ipf
+
+
+
+
+csv = 'filename.csv'
+csv = pd.read_csv(csv)
+print(csv)
+# Ensure points columns exist.
+if 'Wilks' not in csv.columns:
+    csv['Wilks'] = csv
+# if 'McCulloch' not in csv.columns:
+#     csv.append_column('McCulloch')
+# if 'Glossbrenner' not in csv.columns:
+#     csv.append_column('Glossbrenner')
+# if 'IPFPoints' not in csv.columns:
+#     csv.append_column('IPFPoints')
+
+print(csv)
+
+# indexSex = csv.index('Sex')
+# indexEquipment = csv.index('Equipment')
+# indexEvent = csv.index('Event')
+# indexAge = csv.index('Age')
+# indexBodyweight = csv.index('BodyweightKg')
+# indexTotal = csv.index('TotalKg')
+# indexWilks = csv.index('Wilks')
+# indexMcCulloch = csv.index('McCulloch')
+# indexGlossbrenner = csv.index('Glossbrenner')
+# indexIPFPoints = csv.index('IPFPoints')
+
+# for row in csv.rows:
+#     sex = row[indexSex]
+#     bodyweight = row[indexBodyweight]
+#     total = row[indexTotal]
+
+#     if sex not in ['M', 'F']:
+#         continue
+
+#     if not bodyweight:
+#         continue
+#     bodyweight = float(bodyweight)
+
+#     if not total:
+#         continue
+#     total = float(total)
+
+#     # Add the Wilks score to the row.
+#     score = wilks(sex == 'M', bodyweight, total)
+#     row[indexWilks] = to_string(score)
+
+#     # Calculate the age-adusted score.
+#     age = row[indexAge].replace('.5', '')  # Round down when unknown.
+#     if is_int(age):
+#         row[indexMcCulloch] = to_string(
+#             mcculloch(sex == 'M', int(age), bodyweight, total))
+#     else:
+#         # Better than just leaving it blank, when we have some data.
+#         row[indexMcCulloch] = row[indexWilks]
+
+#     # Add the Glossbrenner score to the row.
+#     gloss = glossbrenner(sex == 'M', bodyweight, total)
+#     row[indexGlossbrenner] = to_string(gloss)
+
+#     # Add the IPF Points to the row.
+#     ipfpoints = ipf(sex, row[indexEquipment], row[indexEvent], bodyweight, total)
+#     row[indexIPFPoints] = to_string(ipfpoints)
+
+# csv.write_filename(filename)
+    
