c18e7f5a9cd12b737163cdd4059afa14bb72a1b7
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-26 13:39:41 +0800

    Update IPFGL-PointsCalculator.py

--- pages/IPFGL-PointsCalculator.py
+++ pages/IPFGL-PointsCalculator.py
@@ -1,3 +1,4 @@
+from tkinter import S
 import pandas as pd
 import sys
 import math
@@ -78,3 +79,16 @@ if result:
 
 if result:
     st.write(ipf1(sex,equip,event,bw,totalkg))
+
+col1, col2, col3 = st.columns(3,gap='Medium')
+with col1:
+    s = st.slider('Squat', 0, 500)
+with col2:
+    b = st.slider('Bench', 0, 500)
+with col3:
+    d = st.slider('Deadlift', 0, 500)
+
+if result:
+    st.write(s+b+d)
+
+
