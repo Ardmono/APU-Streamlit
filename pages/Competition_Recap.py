36da435946d98867e17be9af52bafd681e1ac8d8
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-28 08:55:51 +0800

    Update Competition_Recap.py

--- pages/Competition_Recap.py
+++ pages/Competition_Recap.py
@@ -163,7 +163,9 @@ fig = go.Figure(data=[
 lengthy = len(df_exec.query('Deadlift3Kgfail > -1'))
 fig.update_layout(barmode='stack')
 fig.update_layout(yaxis_range=[0,lengthy])
-
+fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
+                  marker_line_width=1.5, opacity=0.6)
+fig.update_layout(title_text='Stacked graph of lifts by success/fail')
 #fig.show()
 
 df_delection = df_exec.reset_index(drop=True).sort_values(by = ['IPFGL'], ascending = [False])
