3a1f06c573d58768d9d040eb74486aca3e5931ca
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-28 13:11:42 +0800

    Update Competition_Recap.py

--- pages/Competition_Recap.py
+++ pages/Competition_Recap.py
@@ -126,24 +126,18 @@ fails.loc[6,['Lift','Succeed','Fail','Attempted','Percentage']] = ['Deadlift1',(
 fails.loc[7,['Lift','Succeed','Fail','Attempted','Percentage']] = ['Deadlift2',(len(df_exec.query('Deadlift2Kgfail < 1'))),(len(df_exec.query('Deadlift2Kgfail > 0'))),(len(df_exec.query('Deadlift2Kgfail > -1'))),round((len(df_exec.query('Deadlift2Kgfail < 1'))) / (len(df_exec.query('Deadlift2Kgfail > -1'))) * 100,2)]
 fails.loc[8,['Lift','Succeed','Fail','Attempted','Percentage']] = ['Deadlift3',(len(df_exec.query('Deadlift3Kgfail < 1'))),(len(df_exec.query('Deadlift3Kgfail > 0'))),(len(df_exec.query('Deadlift3Kgfail > -1'))),round((len(df_exec.query('Deadlift3Kgfail < 1'))) / (len(df_exec.query('Deadlift3Kgfail > -1'))) * 100,2)]
 
-testyi = fails['Percentage'].min()
-#print(testyi)
-failedlift = fails[fails['Percentage'] == testyi]
-#tester = fails.iloc[int(failedlift)]
-#print(tester)
-#print(failedlift[['Lift']].to_string(index=False))
-#print(failedlift.to_string(index=False))
-st.text(failedlift[['Lift']])
-blut = failedlift[['Lift']].to_string(index=False)
-blut = blut[4:]
-print(blut)
+# testyi = fails['Percentage'].min()
+# #print(testyi)
+# failedlift = fails[fails['Percentage'] == testyi]
+# failedlift.columns = [''] * len(df.columns)
+# print(failedlift[['Lift']])
 
 st.text('Current Comp: '+str(meeter))
 col1, col2, col3,col4,col5,col33 = st.columns(6,gap='Medium')
 col1.metric(label="How Many Lifters", value=len(df_exec.query('Bench3Kgfail > 0')))
 col2.metric(label="Gender Ratios", value='Bleh')
 col3.metric(label="% Succesful Lifts", value='Bleh')
-col33.metric(label="Most failed lift", value=blut,delta=('-'+str(testyi)+'%'))
+col33.metric(label="Most failed lift", value=blut)
 col4.metric(label="Total KG of all lifted weights", value=(str(df_exec['TotalKg'].sum())+' Kg'))
 col5.metric(label="Count of 9/9 Lifts",value=len(df_exec.query('Squat3Kg > 1 & Squat2Kg > 1 & Squat1Kg > 1 & Bench1Kg > 1 & Bench2Kg > 1 & Bench3Kg > 1 & Deadlift1Kg > 1 & Deadlift2Kg > 1 & Deadlift3Kg > 1')))
 
