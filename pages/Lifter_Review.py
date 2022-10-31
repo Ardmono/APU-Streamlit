c3e1c75025dacf16e55d7ba89efec8cfde766fe7
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-31 10:36:28 +0800

    Update Lifter_Review.py

--- pages/Lifter_Review.py
+++ pages/Lifter_Review.py
@@ -36,7 +36,7 @@ fig2 = px.bar(df, x='Name', y=['Best3BenchKg', 'Best3SquatKg', 'Best3DeadliftKg'
 
 
     
-fig2.update_layout(width=500)
+fig2.update_layout(width=2000)
 print(df.columns)
 
 st.write(fig2)
