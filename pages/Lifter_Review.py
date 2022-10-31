30980ddef78bc1a4b1e137b9bd60c71587334b15
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-31 09:35:33 +0800

    Update Lifter_Review.py

--- pages/Lifter_Review.py
+++ pages/Lifter_Review.py
@@ -32,7 +32,7 @@ maxipfgl = int(maxipfgl)
 maxipfgl = maxipfgl + 10
 dateoptions = df['Date'].unique().tolist()
 print(dateoptions)
-fig2 = px.bar(df, x='Name', y='IPFGL', animation_frame=dateoptions, range_y=[0,maxipfgl])
+fig2 = px.bar(df, x='Name', y=['Best3SquatKg', 'Best3BenchKg'], animation_frame=dateoptions, range_y=[0,maxipfgl])
 
 
 
