c2bcabef81bd66b86edb4322bb3cdb5b21149e40
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-10-31 09:05:15 +0800

    Update Lifter_Review.py

--- pages/Lifter_Review.py
+++ pages/Lifter_Review.py
@@ -1,4 +1,5 @@
 from time import strptime
+from turtle import width
 
 import pandas as pd 
 import streamlit as st
@@ -6,6 +7,7 @@ from datetime import datetime
 import time
 import numpy as np
 import plotly.express as px
+import plotly
 
 df = 'https://raw.githubusercontent.com/Ardmono/APU-Streamlit/main/filename.csv'
 df = pd.read_csv(df)
@@ -31,7 +33,10 @@ dateoptions = df['Date'].unique().tolist()
 fig2 = px.bar(df, x='Name', y='IPFGL', animation_frame='Date', animation_group='Name')
 
 
-fig2.update_layout(width=800)
+
+fig2.update_layout(width=500)
+
+
 st.write(fig2)
 #print(df['Name'].value_counts())
 
