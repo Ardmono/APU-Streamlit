c537a4c812a42c4f7e41f1faeb857a309d8cd6d0
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-11-01 14:56:54 +0800

    Update IPFGL-PointsCalculator.py

--- pages/IPFGL-PointsCalculator.py
+++ pages/IPFGL-PointsCalculator.py
@@ -139,16 +139,18 @@ df_delection3 = df_delection[df_delection['Sex'] == sex]
 if sex == 'M':
     if bw > 66 and  bw < 74:
         wc = '74'
-        df_delection3 = df_delection3[df_delection3['WeightClassKg'] == wc]
+        #df_delection3 = df_delection3[df_delection3['WeightClassKg'] == wc]
     else:
         pass
+    df_delection3 = df_delection3[df_delection3['WeightClassKg'] == wc]
     #if df_delection[df_delection['WeightClassKg'] == sex]
 if sex == 'F':
     if bw > 57 and  bw < 63:
         wc = '63'
-        df_delection3 = df_delection3[df_delection3['WeightClassKg'] == wc]
+        #df_delection3 = df_delection3[df_delection3['WeightClassKg'] == wc]
     else:
         pass
+    df_delection3 = df_delection3[df_delection3['WeightClassKg'] == wc]
     #    wc = df['WeightClassKg'].unique()
     #df_delection3 = df_delection3[df_delection3['WeightClassKg'] == wc]
 
