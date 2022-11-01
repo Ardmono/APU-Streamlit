5c1d7d7673d537ccb521535933cbdef875e66403
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-11-01 14:05:38 +0800

    Update IPFGL-PointsCalculator.py

--- pages/IPFGL-PointsCalculator.py
+++ pages/IPFGL-PointsCalculator.py
@@ -132,7 +132,7 @@ st.text(ipfglp)
 ranker = int(df_delection.loc[df_delection['IPFGL'] <= ipfglp].index[0])
 
 df_delection2 = df_delection[df_delection['Sex' == sex]]
-
+st.text(sex)
 ranker2 = int(df_delection2.loc[df_delection2['IPFGL'] <= ipfglp].index[0])
 st.text(ranker)
 
